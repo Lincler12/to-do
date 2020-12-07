eeaac9c2c816f4c3d296552591188218a7b1b03a
Icarus <astrikort24@gmail.com>
2020-12-07 20:05:28 +0200

    added input type text for adding project

---
+++ ui-project-toggle.js
@@ -0,0 +1,16 @@
+const toggleProjectButton = document.getElementById("toggle-projects-btn");
+const projectsUl = document.querySelector("div.sidenav ul");
+const imag1 = new Image();
+imag1.src = `./images/icons8-expand-arrow-16.png`;
+const imag2 = new Image();
+imag2.src = `./images/icons8-collapse-arrow-16.png`;
+function toggleProjects() {
+  projectsUl.classList.toggle("project-wrapper-off");
+  if (toggleProjectButton.src === imag1.src) {
+    toggleProjectButton.src = "./images/icons8-collapse-arrow-16.png";
+  } else {
+    toggleProjectButton.src = "./images/icons8-expand-arrow-16.png";
+  }
+}
+
+toggleProjectButton.addEventListener("click", toggleProjects);
