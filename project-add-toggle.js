6506e28e7417f37d3aaea8a57715419e1425e850
Icarus <astrikort24@gmail.com>
2020-12-08 11:31:11 +0200

    clicking outside of form now closes it if it's open

---
+++ project-add-toggle.js
@@ -0,0 +1,28 @@
+const getProjectName = document.getElementById("get-project-name");
+const addProjectBtn = document.getElementById("add-project-btn");
+const closeBtn = getProjectName.querySelector(".close-btn");
+const input = document.getElementById("project-name");
+
+function toggleProjectNameInput() {
+  getProjectName.classList.toggle("get-project-name-off");
+}
+
+function closeProjectInputName() {
+  input.value = "";
+  toggleProjectNameInput();
+}
+
+//on click in document.body close input form if open
+window.addEventListener("click", (e) => {
+  if (
+    e.target !== addProjectBtn &&
+    !getProjectName.classList.contains("get-project-name-off")
+  ) {
+    closeProjectInputName();
+  }
+});
+getProjectName.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+addProjectBtn.addEventListener("click", toggleProjectNameInput);
+closeBtn.addEventListener("click", closeProjectInputName);
