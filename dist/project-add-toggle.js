f74e18c5806caa36e1a986bb64004ba821ced074
Icarus <astrikort24@gmail.com>
2020-12-08 22:49:21 +0200

    Added modal DOM element and added utility to plus button on top bar

--- dist/project-add-toggle.js
+++ dist/project-add-toggle.js
@@ -1,28 +1,30 @@
-const getProjectName = document.getElementById("get-project-name");
-const addProjectBtn = document.getElementById("add-project-btn");
-const closeBtn = getProjectName.querySelector(".close-btn");
-const input = document.getElementById("project-name");
+const projectAddToggle = () => {
+  const getProjectName = document.getElementById("get-project-name");
+  const addProjectBtn = document.getElementById("add-project-btn");
+  const closeBtn = getProjectName.querySelector(".close-btn");
+  const input = document.getElementById("project-name");
 
-function toggleProjectNameInput() {
-  getProjectName.classList.toggle("get-project-name-off");
-}
-
-function closeProjectInputName() {
-  input.value = "";
-  toggleProjectNameInput();
-}
-
-//on click in document.body close input form if open
-window.addEventListener("click", (e) => {
-  if (
-    e.target !== addProjectBtn &&
-    !getProjectName.classList.contains("get-project-name-off")
-  ) {
-    closeProjectInputName();
+  function toggleProjectNameInput() {
+    getProjectName.classList.toggle("get-project-name-off");
   }
-});
-getProjectName.addEventListener("click", (e) => {
-  e.stopPropagation();
-});
-addProjectBtn.addEventListener("click", toggleProjectNameInput);
-closeBtn.addEventListener("click", closeProjectInputName);
+
+  function closeProjectInputName() {
+    input.value = "";
+    toggleProjectNameInput();
+  }
+
+  //on click in document.body close input form if open
+  window.addEventListener("click", (e) => {
+    if (
+      e.target !== addProjectBtn &&
+      !getProjectName.classList.contains("get-project-name-off")
+    ) {
+      closeProjectInputName();
+    }
+  });
+  getProjectName.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+  addProjectBtn.addEventListener("click", toggleProjectNameInput);
+  closeBtn.addEventListener("click", closeProjectInputName);
+};
